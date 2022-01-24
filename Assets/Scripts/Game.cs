89bf6f32d86bdc41b2f74ee07de12ca5b5172553
YasirQuadri <47654258+YasirQuadri@users.noreply.github.com>
2022-01-24 19:27:29 +0530

    added new rules pdf and updated prototype ui

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    public GameObject unit;
+    public GameObject fort;
+
+    void Start()
+    {
+        Instantiate(unit, new Vector3(0, 0, 0), Quaternion.identity);
+        Instantiate(fort, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+}
---
+++ Assets/Scripts/Units.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Units : MonoBehaviour
+{
+    // References
+    public GameObject controller;
+    public GameObject moveBlock;
+
+    // Positions
+    int xPosition = -1;
+    int yPosition = -1;
+
+    // Variable to track red ,green, blue, yellow Players
+    string player;
+
+    //reference for sprites of units
+    public Sprite BlueUnit, RedUnit, GreenUnit, YellowUnit;
+}
